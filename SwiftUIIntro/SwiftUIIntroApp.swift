7ca66a28fb8f3012af1a3e9cb138f39e97aa4a59
Fulya Sariyar <fulyasariyar@outlook.com>
2022-06-14 12:28:37 +0300

    init

---
+++ SwiftUIIntro/SwiftUIIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntroApp.swift
+//  SwiftUIIntro
+//
+//  Created by Fulya Sarıyar on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
